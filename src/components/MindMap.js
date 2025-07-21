8cadb0d744dcf1ab485bd639c0f1ecd82322f6a5
Gen-direktor <cool.generalny@yandex.ru>
2025-07-21 13:44:21 +0300

    Update MindMap.js

--- src/components/MindMap.js
+++ src/components/MindMap.js
@@ -1,1 +1,19 @@
+export function initMindMap() {
+    const mindmap = document.getElementById('mindmap');
+    
+    function addNode(element) {
+        // Логика добавления узла
+    }
+    
+    function deleteNode(element) {
+        // Логика удаления узла
+    }
+    
+    function editNode(element) {
+        // Логика редактирования узла
+    }
+    
+    // Добавляем обработчики событий
+    mindmap.addEventListener('click', handleClick);
+}
 
